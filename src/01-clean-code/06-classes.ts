39a3032742bdd9195ff4de1a2a93eb9bc348790e
devferx <ferkki@hotmail.com>
2024-01-30 17:44:10 -0400

    Herencia - Problemática

--- src/01-clean-code/06-classes.ts
+++ src/01-clean-code/06-classes.ts
@@ -1,6 +1,8 @@
 (() => {
   type Gender = 'M' | 'F';
 
+  // No aplicando el principio de responsabilidad única
+
   class Person {
     constructor(
       public name: string,
@@ -9,6 +11,51 @@
     ) {}
   }
 
-  const newPerson = new Person('John Doe', 'M', new Date('1990-01-01'));
-  console.log(newPerson);
+  class User extends Person {
+    public lastAccess: Date;
+
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date,
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date,
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+  }
+
+  const userSettings = new UserSettings(
+    '/usr/home',
+    '/home',
+    'fer@gmail.com',
+    'admin',
+    'Fernando',
+    'M',
+    new Date(2002, 5, 8),
+  );
+
+  console.log({
+    userSettings,
+    areaCredentialsValid: userSettings.checkCredentials(),
+  });
 })();
